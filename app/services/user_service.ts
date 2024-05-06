7170da6f1c8737aa6c2146bbdbc8394ca3f01635
Somtosoft <a025073m@student.staffs.ac.uk>
2024-05-06 02:58:45 +0100

    Refactor AuthService, UserService, and WalletService to improve code readability, security, and remove unused code

--- app/services/user_service.ts
+++ app/services/user_service.ts
@@ -5,14 +5,13 @@ import { randomUniqueNumbersInRange, toSentenceCase } from '../utils/common.js'
 import {
   containsOnlyAlphabets,
   containsOnlyAlphabetsAndNumbers,
-  validatePIN,
-  validatePINChallengeLength,
+  validatePIN, 
 } from '../utils/validator.js'
 import hash from '@adonisjs/core/services/hash'
 import QuestionService from '#services/question_service'
 import { nanoid } from 'nanoid'
 import WalletService from '#services/wallet_service'
-import { ACCOUNT, MENURESPONSE, USSDMENUOPTIONS } from '../utils/constants.js'
+import { ACCOUNT } from '../utils/constants.js'
 
 export default class UserService {
   response: string
@@ -167,137 +166,137 @@ export default class UserService {
     return this.response
   }
 
-  async loginUser(request: ILoginUserDto): Promise<string> {
-    const user = await User.findBy('phoneNumber', request.phoneNumber)
-    if (!user) {
-      this.response = 'END You are not registered, Register to continue.'
-      return this.response
-    }
-    // is User Blocked
-    if (user.status === ACCOUNT.BLOCKED) {
-      this.response = `${MENURESPONSE.END} Your account has been blocked. Dial ${process.env.SERVICE_CODE} to unblock it.`
-      return this.response
-    }
-    /*START PIN CHALLENGE VERIFICATION*/
-    // 1. check if random positions have been set for the user
-    let cachedChallengePositions = await redis.get(`position-${user.id}`)
-    let challengePositions: number[] = []
-    const parsedPassword = JSON.parse(user?.password)
-    // 2. if not, generate random positions and set in redis
-    if (!cachedChallengePositions) {
-      challengePositions = this.generateChallenge(parsedPassword.length)
-      await redis.set(`position-${user.id}`, JSON.stringify(challengePositions))
-    } else {
-      challengePositions = JSON.parse(cachedChallengePositions)
-    }
-    if (!request.pin) {
-      this.response = `CON Enter the ${challengePositions.map((position) => this.changeNumberToPositionWord(position)).join(' and ')} characters of your pin`
-      return this.response
-    }
-    // 3. validate the length of the pin
-    if (!validatePINChallengeLength(request.pin)) {
-      this.response = 'END Invalid characters, try again.'
-      return this.response
-    }
-    // 4. verify the characters
-    else {
-      let retries = await redis.get(`retries-${request.phoneNumber}`)
-      if (retries && parseInt(retries) >= 3) {
-        user.status = ACCOUNT.BLOCKED as keyof typeof ACCOUNT
-        await user.save()
-        this.response =
-          'END You have exceeded the maximum number of retries.\n Your account has been locked.\n Unlock it by answeing your security questions.'
-        return this.response
-      }
+  // async loginUser(request: ILoginUserDto): Promise<string> {
+  //   const user = await User.findBy('phoneNumber', request.phoneNumber)
+  //   if (!user) {
+  //     this.response = 'END You are not registered, Register to continue.'
+  //     return this.response
+  //   }
+  //   // is User Blocked
+  //   if (user.status === ACCOUNT.BLOCKED) {
+  //     this.response = `${MENURESPONSE.END} Your account has been blocked. Dial ${process.env.SERVICE_CODE} to unblock it.`
+  //     return this.response
+  //   }
+  //   /*START PIN CHALLENGE VERIFICATION*/
+  //   // 1. check if random positions have been set for the user
+  //   let cachedChallengePositions = await redis.get(`position-${user.id}`)
+  //   let challengePositions: number[] = []
+  //   const parsedPassword = JSON.parse(user?.password)
+  //   // 2. if not, generate random positions and set in redis
+  //   if (!cachedChallengePositions) {
+  //     challengePositions = this.generateChallenge(parsedPassword.length)
+  //     await redis.set(`position-${user.id}`, JSON.stringify(challengePositions))
+  //   } else {
+  //     challengePositions = JSON.parse(cachedChallengePositions)
+  //   }
+  //   if (!request.pin) {
+  //     this.response = `CON Enter the ${challengePositions.map((position) => this.changeNumberToPositionWord(position)).join(' and ')} characters of your pin`
+  //     return this.response
+  //   }
+  //   // 3. validate the length of the pin
+  //   if (!validatePINChallengeLength(request.pin)) {
+  //     this.response = 'END Invalid characters, try again.'
+  //     return this.response
+  //   }
+  //   // 4. verify the characters
+  //   else {
+  //     let retries = await redis.get(`retries-${request.phoneNumber}`)
+  //     if (retries && parseInt(retries) >= 3) {
+  //       user.status = ACCOUNT.BLOCKED as keyof typeof ACCOUNT
+  //       await user.save()
+  //       this.response =
+  //         'END You have exceeded the maximum number of retries.\n Your account has been locked.\n Unlock it by answeing your security questions.'
+  //       return this.response
+  //     }
 
-      for (let i = 0; i < challengePositions.length; i++) {
-        const verifiedChar = await hash.verify(
-          parsedPassword[challengePositions[i]],
-          request.pin[i]
-        )
-        if (!verifiedChar) {
-          // INCREMENT RETRIES
-          if (retries) {
-            await redis.set(`retries-${request.phoneNumber}`, parseInt(retries) + 1)
-          } else {
-            await redis.set(`retries-${request.phoneNumber}`, 1)
-          }
-          this.response = 'END Incorrect characters, try again.'
-          return this.response
-        } else {
-          await redis.del(`retries-${request.phoneNumber}`)
-          await redis.del(`position-${user.id}`)
-          console.log('successful login')
-          this.response = 'SUCCESS'
-          return this.response
-        }
-      }
-    }
-    return this.response
-  }
+  //     for (let i = 0; i < challengePositions.length; i++) {
+  //       const verifiedChar = await hash.verify(
+  //         parsedPassword[challengePositions[i]],
+  //         request.pin[i]
+  //       )
+  //       if (!verifiedChar) {
+  //         // INCREMENT RETRIES
+  //         if (retries) {
+  //           await redis.set(`retries-${request.phoneNumber}`, parseInt(retries) + 1)
+  //         } else {
+  //           await redis.set(`retries-${request.phoneNumber}`, 1)
+  //         }
+  //         this.response = 'END Incorrect characters, try again.'
+  //         return this.response
+  //       } else {
+  //         await redis.del(`retries-${request.phoneNumber}`)
+  //         await redis.del(`position-${user.id}`)
+  //         console.log('successful login')
+  //         this.response = 'SUCCESS'
+  //         return this.response
+  //       }
+  //     }
+  //   }
+  //   return this.response
+  // }
 
-  async unlockAccount(request: IUSSDRequest): Promise<string> {
-    // so check if the user exists
-    const user = await User.findBy('phoneNumber', request.phoneNumber)
-    if (!user) {
-      this.response = 'END Invalid option selected. Please try again.'
-      return this.response
-    }
-    // check if the user is blocked
-    if (user.status === ACCOUNT.ACTIVE) {
-      this.response = 'END Your account is not blocked.'
-      return this.response
-    } else {
-      const response = request.text.split('*')
-      const securityQuestions = JSON.parse(user.securityQuestions)
-      let randomPosition = 0
-      if (response.length === 1) {
-        // check if random question position has been set
-        let selectedQuestion = await redis.get(`selectedQuestion-${request.phoneNumber}`)
+  // async unlockAccount(request: IUSSDRequest): Promise<string> {
+  //   // so check if the user exists
+  //   const user = await User.findBy('phoneNumber', request.phoneNumber)
+  //   if (!user) {
+  //     this.response = 'END Invalid option selected. Please try again.'
+  //     return this.response
+  //   }
+  //   // check if the user is blocked
+  //   if (user.status === ACCOUNT.ACTIVE) {
+  //     this.response = 'END Your account is not blocked.'
+  //     return this.response
+  //   } else {
+  //     const response = request.text.split('*')
+  //     const securityQuestions = JSON.parse(user.securityQuestions)
+  //     let randomPosition = 0
+  //     if (response.length === 1) {
+  //       // check if random question position has been set
+  //       let selectedQuestion = await redis.get(`selectedQuestion-${request.phoneNumber}`)
 
-        if (!selectedQuestion) {
-          randomPosition = Math.floor(Math.random() * securityQuestions.length)
-          selectedQuestion = await redis.set(
-            `selectedQuestion-${request.phoneNumber}`,
-            randomPosition
-          )
-          const question = securityQuestions[randomPosition]
-          this.response = `CON You Need to answer your security questions to unlock your account.\n${question.question}`
-          return this.response
-        } else {
-          randomPosition = parseInt(selectedQuestion)
-          const question = securityQuestions[randomPosition]
-          this.response = `CON You Need to answer your security questions to unlock your account.\n${question.question}`
-          return this.response
-        }
-      }
-      if (response.length === 2) {
-        let selectedQuestion = await redis.get(`selectedQuestion-${request.phoneNumber}`)
-        if (!selectedQuestion) {
-          selectedQuestion = await redis.set(
-            `selectedQuestion-${request.phoneNumber}`,
-            randomPosition
-          )
-        }
-        const question = securityQuestions[selectedQuestion!]
-        if (!containsOnlyAlphabetsAndNumbers(response[1])) {
-          this.response = 'END Invalid answer format. Please try again.'
-          return this.response
-        }
-        if (toSentenceCase(response[1]) !== question.answer) {
-          this.response = 'END Incorrect answer. Please try again.'
-          return this.response
-        }
-        await redis.del(`retries-${request.phoneNumber}`)
-        await redis.del(`selectedQuestion-${request.phoneNumber}`)
-        user.status = ACCOUNT.ACTIVE as keyof typeof ACCOUNT
-        await user.save()
-        this.response = 'END Account unlocked successfully.'
-        return this.response
-      }
-      return this.response
-    }
-  }
+  //       if (!selectedQuestion) {
+  //         randomPosition = Math.floor(Math.random() * securityQuestions.length)
+  //         selectedQuestion = await redis.set(
+  //           `selectedQuestion-${request.phoneNumber}`,
+  //           randomPosition
+  //         )
+  //         const question = securityQuestions[randomPosition]
+  //         this.response = `CON You Need to answer your security questions to unlock your account.\n${question.question}`
+  //         return this.response
+  //       } else {
+  //         randomPosition = parseInt(selectedQuestion)
+  //         const question = securityQuestions[randomPosition]
+  //         this.response = `CON You Need to answer your security questions to unlock your account.\n${question.question}`
+  //         return this.response
+  //       }
+  //     }
+  //     if (response.length === 2) {
+  //       let selectedQuestion = await redis.get(`selectedQuestion-${request.phoneNumber}`)
+  //       if (!selectedQuestion) {
+  //         selectedQuestion = await redis.set(
+  //           `selectedQuestion-${request.phoneNumber}`,
+  //           randomPosition
+  //         )
+  //       }
+  //       const question = securityQuestions[selectedQuestion!]
+  //       if (!containsOnlyAlphabetsAndNumbers(response[1])) {
+  //         this.response = 'END Invalid answer format. Please try again.'
+  //         return this.response
+  //       }
+  //       if (toSentenceCase(response[1]) !== question.answer) {
+  //         this.response = 'END Incorrect answer. Please try again.'
+  //         return this.response
+  //       }
+  //       await redis.del(`retries-${request.phoneNumber}`)
+  //       await redis.del(`selectedQuestion-${request.phoneNumber}`)
+  //       user.status = ACCOUNT.ACTIVE as keyof typeof ACCOUNT
+  //       await user.save()
+  //       this.response = 'END Account unlocked successfully.'
+  //       return this.response
+  //     }
+  //     return this.response
+  //   }
+  // }
 
   private async hashPassword(password: string): Promise<string> {
     const charHashes = []
@@ -306,24 +305,24 @@ export default class UserService {
     }
     return JSON.stringify(charHashes)
   }
-  private generateChallenge(wordLength: number): number[] {
-    const positions: number[] = []
-    let attempts = 0
-    while (positions.length < 3 && attempts < 10) {
-      const position = Math.floor(Math.random() * (wordLength - 1)) + 1 // Exclude the first character for security
-      if (!positions.includes(position)) {
-        positions.push(position)
-      }
-      attempts++
-    }
+  // private generateChallenge(wordLength: number): number[] {
+  //   const positions: number[] = []
+  //   let attempts = 0
+  //   while (positions.length < 3 && attempts < 10) {
+  //     const position = Math.floor(Math.random() * (wordLength - 1)) + 1 // Exclude the first character for security
+  //     if (!positions.includes(position)) {
+  //       positions.push(position)
+  //     }
+  //     attempts++
+  //   }
 
-    return positions
-  }
+  //   return positions
+  // }
 
-  private changeNumberToPositionWord(number: number) {
-    const words = ['first', 'second', 'third', 'fourth', 'fifth', 'sixth', 'seventh', 'eighth']
-    return words[number]
-  }
+  // private changeNumberToPositionWord(number: number) {
+  //   const words = ['first', 'second', 'third', 'fourth', 'fifth', 'sixth', 'seventh', 'eighth']
+  //   return words[number]
+  // }
 
   private async createNewUser(userData: IUserDto) {
     try {
